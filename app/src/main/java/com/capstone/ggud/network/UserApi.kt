2ae550bdff440c007c910d2ad020c35ae9646a44
eunwoo031217 <eunwoo031217@users.noreply.github.com>
2026-05-15 14:29:15 +0900

    feat: 프로필 사진 수정 기능 추가

--- app/src/main/java/com/capstone/ggud/network/UserApi.kt
+++ app/src/main/java/com/capstone/ggud/network/UserApi.kt
@@ -2,9 +2,13 @@ package com.capstone.ggud.network
 
 import com.capstone.ggud.network.dto.UpdateProfileRequest
 import com.capstone.ggud.network.dto.UserResponse
+import okhttp3.MultipartBody
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Multipart
 import retrofit2.http.PATCH
+import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface UserApi {
 
@@ -14,5 +18,11 @@ interface UserApi {
     @PATCH("/api/v1/users/me")
     suspend fun updateMy(
         @Body request: UpdateProfileRequest
+    ): UserResponse
+
+    @Multipart
+    @POST("/api/v1/users/me/profile-image")
+    suspend fun uploadProfileImage(
+        @Part image: MultipartBody.Part
     ): UserResponse
 }
